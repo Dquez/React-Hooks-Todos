6b2abc871b6aea7aceecdf55837968000a8b7b39
Dariell Vasquez <dariellv@yahoo.com>
2018-12-18 14:46:34 -0500

    UserList component using reusable hook component

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,18 @@
 import React, {useState} from 'react';
 import ResourceList from '../components/ResourceList';
+import UserList from '../components/UserList';
 
 export default ()=>{
     const [resource, setResource] = useState('posts');
         return(
             <div>
+                <UserList />
                 <div>
                     <button onClick={()=>setResource('posts')}>Post</button>
                     <button onClick={()=>setResource('todos')}>Todos</button>
                 </div>
                 <ResourceList resource={resource}/>
+                
             </div>
         )
 
